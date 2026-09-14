535540ae8990cdf4b1421322e126199bea485096
ha547 <116722531+ha547@users.noreply.github.com>
2026-09-14 17:15:46 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,21 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     base: '/Vocabino.com/',
-    plugins: [react(), tailwindcss()],
+
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
       host: '0.0.0.0',
-      allowedHosts: ['site--vocabino-backend--qk6n2px2g77r.code.run'],
+      allowedHosts: true,
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
